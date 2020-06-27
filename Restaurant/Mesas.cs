e225b15b478b95df1516bcb547e39514ec5a1d4b
angelbaezsa <47637565+angelbaezsa@users.noreply.github.com>
2020-06-27 13:45:54 -0400

    Business logic creado y referenciado

--- Restaurant/Mesas.cs
+++ Restaurant/Mesas.cs
@@ -23,5 +23,6 @@ namespace Restaurant
             return numPersonas;
         }
 
+
     }
 }
--- Restaurant/frmOrdenes.Designer.cs
+++ Restaurant/frmOrdenes.Designer.cs
@@ -34,7 +34,11 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtEstadoMesa = new System.Windows.Forms.TextBox();
+            this.btnProcesarOrden = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.tlpMesa.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tlpMesa
@@ -47,14 +51,18 @@
             this.tlpMesa.Controls.Add(this.comboBox1, 1, 1);
             this.tlpMesa.Controls.Add(this.label1, 0, 2);
             this.tlpMesa.Controls.Add(this.txtEstadoMesa, 1, 2);
+            this.tlpMesa.Controls.Add(this.btnProcesarOrden, 1, 4);
+            this.tlpMesa.Controls.Add(this.tableLayoutPanel1, 0, 3);
             this.tlpMesa.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpMesa.Location = new System.Drawing.Point(0, 0);
             this.tlpMesa.Name = "tlpMesa";
-            this.tlpMesa.RowCount = 4;
-            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.90887F));
-            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.31175F));
-            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.393286F));
-            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.90647F));
+            this.tlpMesa.RowCount = 5;
+            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.27273F));
+            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.76596F));
+            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.98176F));
+            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.28701F));
+            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85F));
+            this.tlpMesa.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlpMesa.Size = new System.Drawing.Size(438, 417);
             this.tlpMesa.TabIndex = 0;
             // 
@@ -66,7 +74,7 @@
             this.lblNoClientes.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNoClientes.Location = new System.Drawing.Point(3, 0);
             this.lblNoClientes.Name = "lblNoClientes";
-            this.lblNoClientes.Size = new System.Drawing.Size(432, 58);
+            this.lblNoClientes.Size = new System.Drawing.Size(432, 57);
             this.lblNoClientes.TabIndex = 0;
             this.lblNoClientes.Text = "Informacion de la Mesa";
             // 
@@ -75,16 +83,16 @@
             this.lblNumPersonas.AutoSize = true;
             this.lblNumPersonas.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblNumPersonas.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumPersonas.Location = new System.Drawing.Point(3, 58);
+            this.lblNumPersonas.Location = new System.Drawing.Point(3, 57);
             this.lblNumPersonas.Name = "lblNumPersonas";
-            this.lblNumPersonas.Size = new System.Drawing.Size(225, 43);
+            this.lblNumPersonas.Size = new System.Drawing.Size(225, 42);
             this.lblNumPersonas.TabIndex = 1;
             this.lblNumPersonas.Text = "Numero de personas";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(234, 61);
+            this.comboBox1.Location = new System.Drawing.Point(234, 60);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(178, 21);
             this.comboBox1.TabIndex = 2;
@@ -94,19 +102,54 @@
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 101);
+            this.label1.Location = new System.Drawing.Point(3, 99);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 35);
+            this.label1.Size = new System.Drawing.Size(225, 46);
             this.label1.TabIndex = 3;
             this.label1.Text = "Estado Mesa";
             // 
             // txtEstadoMesa
             // 
-            this.txtEstadoMesa.Location = new System.Drawing.Point(234, 104);
+            this.txtEstadoMesa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEstadoMesa.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstadoMesa.Location = new System.Drawing.Point(234, 102);
             this.txtEstadoMesa.Name = "txtEstadoMesa";
             this.txtEstadoMesa.ReadOnly = true;
-            this.txtEstadoMesa.Size = new System.Drawing.Size(178, 20);
+            this.txtEstadoMesa.Size = new System.Drawing.Size(201, 29);
             this.txtEstadoMesa.TabIndex = 4;
+            this.txtEstadoMesa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnProcesarOrden
+            // 
+            this.btnProcesarOrden.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnProcesarOrden.Location = new System.Drawing.Point(234, 332);
+            this.btnProcesarOrden.Name = "btnProcesarOrden";
+            this.btnProcesarOrden.Size = new System.Drawing.Size(201, 82);
+            this.btnProcesarOrden.TabIndex = 5;
+            this.btnProcesarOrden.Text = "Procesar Orden";
+            this.btnProcesarOrden.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tlpMesa.SetColumnSpan(this.tableLayoutPanel1, 2);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 148);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(432, 178);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(426, 172);
+            this.listBox1.TabIndex = 0;
             // 
             // frmOrdenes
             // 
@@ -122,6 +165,7 @@
             this.Load += new System.EventHandler(this.frmOrdenes_Load);
             this.tlpMesa.ResumeLayout(false);
             this.tlpMesa.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -134,5 +178,8 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtEstadoMesa;
+        private System.Windows.Forms.Button btnProcesarOrden;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
